0392d566193b89915122b1f1e37ca879ba83f8d6
NESSMA <nessma.hamidou@insat.u-carthage.tn>
2023-02-13 20:27:57 +0100

    propos link

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -5,7 +5,7 @@ function NavLinks() {
     <div className="navLinks-container " >
         <ul className="navLinks">
             <a href="#"><span>Accueil</span></a>
-            <a href=""><span>A propos</span></a>
+            <a href="#nous-contacter"><span>A propos</span></a>
             <a href="#chambres"><span>Nos chambres</span></a>
             <a href="#pt-dej"><span>Notre petit déjeuner</span></a>  
             <a href="#nous-contacter"><span>Contact</span></a>
